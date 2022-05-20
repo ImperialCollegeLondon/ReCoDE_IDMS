214e93ddd930fda887a112005d72d73f33b2b5ba
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2022-05-20 14:46:37 +0100

    reformat existing code and add in diagnostics

--- part_1/scripts/fit_model1_solver.R
+++ part_1/scripts/fit_model1_solver.R
@@ -38,59 +38,94 @@ all_dates = seq.Date(from = start_date, to = end_date ,  by = "days")
 # date to seed omicron 
 seed_omicron =  which(all_dates == as.Date.character("01-10-2021", format = "%d-%m-%Y")) 
 
-# model times 
-ts = 1:length(all_dates)
+
+# we fix the rate of progression and rate of recovery 
+# we estimate beta (transmission rate) and rho (reporting probability)
+
+sigma = 1 / 5.1   # rate of progression
+gamma = 1/2.1     # rate of recovery 
+
+# population and seropositive populations are known
+# we must estimate the initial number of infections 
+
+n_pop = 15810388 # population of Guateng 
+n_recov = round(n_pop * 0.562) # 56.2% seropositive  
+
+
+# parameters to estimate are therfore:
+
+pars = c("beta", "rho", "I0")
 
 # modify data into a list format for Rstan ------------------------------------# 
 
 
-stan_data_m1_solv = list(n_days = length(times), # Number of observations 
-                         n_pop = N,
-                         n_recov = R,
-                         y = Inc$Inc[seed_omicron:length(Inc$Inc)],
-                         n_data = length(Inc$Inc[seed_omicron:length(Inc$Inc)]),
-                         ts = times,
+stan_data_m1_solv = list(n_days = dim(sim_data)[1], # Number of observations 
+                         n_pop = n_pop,
+                         n_recov = n_recov,
+                         y = sim_data$rep_inc[seed_omicron:length(ts)],
+                         n_data = length(sim_data$rep_inc[seed_omicron:length(ts)]),
                          sigma = sigma,
                          gamma = gamma,
+                         ts = sim_data$time, 
                          time_seed_omicron = seed_omicron)
 
 
 
-## Here we compile our Rstan model 
+# compile our Rstan model -----------------------------------------------------# 
 
-m1_solv =stan_model( "models/Omicron_model_1_solver.stan")
+m1_solv = stan_model(paste0(file_path,"models/model1_solver.stan"))
 
+
+# run our Rstan model ---------------------------------------------------------# 
 
 time.start <- Sys.time()
 m1_fit_solv = sampling(
   m1_solv,
   data = stan_data_m1_solv,
   init = ini_1,
-  n_chains=3,
-  n_warmups=1000,
-  n_iter=2000,
-  n_thin = 5
+  chains=3,
+  warmup=1000,
+  iter=2000,
+  thin = 5
 )
 time.end <- Sys.time()
 time.end - time.start 
 
 
-## Summary of the parameters we are interested in 
+# We obtained no warnings about our model, great news! 
 
-m1_fit_solv_summary <- summary(m1_fit_solv, pars = c("lp__", "beta", "I0", "R_0", "rho"))$summary
-print(m1_fit_solv_summary,scientific=FALSE,digits=2)
+# To be on the safe side, lets check with some model diagnostics
 
-############## WAIC / LOO  ############## 
+check_divergences(m1_fit_solv) # no divergent transitions 
 
-loom1_solv = loo(m1_fit_solv, save_psis = TRUE)
-
+m1_solv_post= as.array(m1_fit_solv)
 
 
-################################      Plotting the fits ################################     
+# plot markov chain trace plots to check for model convergence 
+
+mcmc_trace(m1_solv_post, pars="lp__")
+mcmc_trace(m1_solv_post, pars=pars)
+mcmc_trace(m1_solv_post, pars="R_0")
+
+
+# Univariate and bivariate marginal posterior distributions
+
+pairs(m1_fit_solv, pars = pars, cex.labels=1.5, font.labels=9, condition = "accept_stat__")  
+
+
+# Kernel density estimates of each Markov chain separately, overlaid
+mcmc_dens_overlay(m1_solv_post, pars=pars)
+
+#Central posterior uncertainty intervals
+mcmc_intervals(m1_solv_post, pars=pars)
+
+
+
+# Plotting the moedl output against the data ----------------------------------#     
 
 m1_fit_solv_post = rstan::extract(m1_fit_solv)
 
-m1_solv_results  = m1_fit_solv_post$lambda %>% as.data.frame.table() %>%
+plot_m1_solv_results  = m1_fit_solv_post$lambda %>% as.data.frame.table() %>%
   rename(ni = iterations, day = Var2, value = Freq) %>%
   dplyr::mutate(
     ni = as.numeric(ni),
@@ -101,12 +136,35 @@ m1_solv_results  = m1_fit_solv_post$lambda %>% as.data.frame.table() %>%
     mean = mean(value),
     upper = quantile(value, 0.975)
   )  %>%  
-  mutate(Time = seed_omicron : length(times)) %>% 
-  left_join(Inc) %>%  
-  ggplot(aes(x = Times, y = Inc)) +
+  mutate(time = seed_omicron : length(ts)) %>% 
+  left_join(sim_data) %>%  
+  ggplot(aes(x = time , y = rep_inc)) +
   geom_point()+
   geom_line(aes(y=mean)) +
   geom_ribbon(aes(ymin=lower,ymax=upper))
+
+
+
+
+# summary  of the parameters we are interested in -----------------------------# 
+
+m1_fit_solv_summary = summary(m1_fit_solv, pars = "pars")$summary
+print(m1_fit_solv_summary,scientific=FALSE,digits=2)
+
+
+
+
+
+# WILL PROBABLY REMOVE THIS AND INTRODUCE IN PART 2 INSTEAD 
+
+# Leave one out cross validation (LOOCV) --------------------------------------#
+
+# If we want to estimate the predictive performance of our model to compare it 
+# with others, for instance to test different biological hypotheses and see 
+# which bests explain the data, we can estimate the theoretical expected log 
+# pointwise predictive density for a new dataset using cross validation. 
+
+loom1_solv = loo(m1_fit_solv, save_psis = TRUE)
 
 
 
