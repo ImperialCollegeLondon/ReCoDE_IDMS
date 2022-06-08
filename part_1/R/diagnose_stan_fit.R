94112d11775c3db82462d315ae1dd33248a97cf6
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2022-06-08 22:30:40 +0100

    Debug plot output

--- part_1/R/diagnose_stan_fit.R
+++ part_1/R/diagnose_stan_fit.R
@@ -8,20 +8,17 @@
 
 # Output: 
 
-# - Number of divergent transitions 
 # - Diagnostic plots
-# - data.frame of summary statistics 
+# - data frame of summary statistics 
 
 diagnose_stan_fit = function(
-stan_fit){
+stan_fit,
+pars){
 
   # required package 
   library(bayesplot)
   library(cowplot)
-  
-  # check divergent transitions: 
-  divergent = check_divergences(stan_fit)
-  
+
 
   stan_fit_post= as.array(stan_fit)
 
@@ -29,16 +26,18 @@ stan_fit){
   markov_trace = mcmc_trace(stan_fit_post, pars=c("lp__", pars))
   
   # bivariate marginal posterior distributions
-  bivar = pairs(m1_fit_euler, pars = pars, cex.labels=1.5, font.labels=9, condition = "accept_stat__")  
+  pairs(stan_fit, pars = pars, cex.labels=1.5, font.labels=9, condition = "accept_stat__")  
   
   # univariate marginal posterior distributions 
-  uni = mcmc_dens_overlay(m1_euler_post, pars=pars)
+  uni = mcmc_dens_overlay(stan_fit, pars=pars)
   
-  plot_out = plot_grid(markov_trace, bivar, uni, ncols = 3)
-
   # summary of parameter values, effective sample size and Rhat 
-  param_sum = summary(m1_fit_euler, pars = pars)$summary
+  param_sum = summary(stan_fit, pars = pars)$summary
 
- return(list(divergent, plot_out, param_sum))
+  # output files 
+  list_out= list( markov_trace,uni, param_sum)
+  names(list_out) = c("markov chain trace plots",
+                      "univariate marginal posterior distributions", "summary statistics of parameters")
+ return(list_out)
 
 }
