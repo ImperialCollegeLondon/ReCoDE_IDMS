04b6f08ca847f21521e2cec7229e8786a076fd0e
Beth Cracknell <bnc19@ic.ac.uk>
2022-06-08 09:43:34 +0100

    rename function to include more explanation

---
+++ part_1/R/draw_init_values.R
@@ -0,0 +1,16 @@
+# Function to generate a different starting values for each Markov chain ------# 
+
+
+# Input:  
+
+# - seed for reproducability 
+
+# Output: 
+
+# - starting values for our 3 model parameters, beta, I0, rho 
+
+draw_init_values = function(seed = 1){
+  set.seed(seed)
+  list(beta=runif(1,1,12),
+       I0 = runif(1,1,200),
+       rho = runif(1,0,1) )}
