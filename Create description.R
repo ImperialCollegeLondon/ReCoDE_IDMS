f167a2400bc2cb2228cb75b4a0f718ef739458b9
Beth Cracknell <bnc19@ic.ac.uk>
2022-06-30 10:23:34 +0100

    Add file to create description.

---
+++ Create description.R
@@ -0,0 +1,19 @@
+library(usethis)
+use_description(check_name = F,
+                roxygen = F,
+                fields = list(
+                  Title = "Bayesian inference for SARS-CoV-2 transmission modelling, using Stan",
+                  Description = "A project to fit a mathematical model of disease transmission to epidemiological data, using R and Stan. 
+                  To learn more about this project, start with README.md",
+                  `Authors@R` =  person("Bethan", "Cracknell Daniels", email = "bnc19@ic.ac.uk", 
+                         role = c("aut", "cre")),
+                  Version = "1.0"
+               )
+)
+usethis::use_package("bayesplot", min_version ="1.8.1")
+usethis::use_package("deSolve" , min_version = "1.2.8")
+usethis::use_package("dplyr", min_version = "1.0.6")
+usethis::use_package("ggplot2" , min_version = "3.3.6")
+usethis::use_package("rstan" , min_version = "2.21.5")
+ 
+usethis::use_package("R" , min_version = "4.0.4", "Depends")
